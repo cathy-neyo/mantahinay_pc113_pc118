a7a4ecc15ba7bd8005c20283786e289efa8bfec4
marklezter29 <ca.mantahinay@mlgcl.edu.ph>
2025-02-17 17:21:44 +0800

    2-17-2025 Activity 1

---
+++ laravel/database/seeders/StudentSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\Student;
+
+class StudentSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // Create 50 students using the factory
+        Student::factory()->count(10)->create();
+    }
+}
