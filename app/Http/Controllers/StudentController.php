9d0367575aa8d2f99a135df3accaa72ba9286b0b
marklezter29 <ca.mantahinay@mlgcl.edu.ph>
2025-02-23 17:30:46 +0800

    02-23-2025 Activity 2.5

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -26,4 +26,68 @@ class StudentController extends Controller
         $students = $query->get();
         return response()->json($students, 200);
     }
+   
+    public function store(Request $request)
+    {
+        $validatedData = $request->validate([
+            'firstname' => 'required|string|max:255',
+            'lastname' => 'required|string|max:255',
+            'age' => 'required|integer',
+            'gender' => 'required|string|max:10',
+            'address' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:students',
+            'contact_number' => 'required|string|max:20',
+            'course' => 'required|string|max:255',
+        ]);
+
+        $student = Student::create($validatedData);
+
+        return response()->json([
+            'message' => 'Student created successfully',
+            'student' => $student,
+        ], 201);
+    }
+
+    public function show(Student $student)
+    {
+        return response()->json($student);
+    }
+
+    public function update(Request $request,$id){
+        $student = Student::find($id);
+        if (is_null($student)) {
+            return response()->json(['message' => 'Student Not Found'], 404);
+        }
+
+        $validatedData = $request->validate([
+            'firstname' => 'required|string|max:255',
+            'lastname' => 'required|string|max:255',
+            'age' => 'required|integer',
+            'gender' => 'required|string|max:10',
+            'address' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:students,email,'.$id,
+            'contact_number' => 'required|string|max:20',
+            'course' => 'required|string|max:255'
+             
+        ]);
+        
+        $student->update($validatedData);
+        return response()->json([
+            'message' => 'Student Updated Successfully',
+            'student' => $student
+        ]);
+        
+    }
+
+    public function destroy($id)
+    {
+        $student = Student::find($id);
+        if (is_null($student)) {
+            return response()->json(['message' => 'Student Not Found'], 404);
+        }
+
+        $student->delete();
+        return response()->json(['message' => 'Student deleted successfully'], 200);
+    }
+    
 }
--- routes/api.php
+++ routes/api.php
@@ -11,3 +11,17 @@
     Route::get('/students/search', [StudentController::class, 'index']);
     Route::get('/employees/search', [EmployeeController::class, 'index']);
 
+    Route::get('employees', [EmployeeController::class, 'index']);
+    Route::post('employees', [EmployeeController::class, 'store']);
+    Route::get('employees/{employee}', [EmployeeController::class, 'show']);
+    Route::put('employees/{employee}', [EmployeeController::class, 'update']);
+    Route::delete('employees/{employee}', [EmployeeController::class, 'destroy']);
+
+    Route::get('students', [StudentController::class, 'index']);
+    Route::post('students', [StudentController::class, 'store']);
+    Route::get('students/{student}', [StudentController::class, 'show']);
+    Route::put('students/{student}', [StudentController::class, 'update']);
+    Route::delete('students/{student}', [StudentController::class, 'destroy']);
+   
+    
+
